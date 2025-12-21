5750cfc1104244d50fcb378c70ce652edf8534ed
DivingDePIN <168707808+DivingDePIN@users.noreply.github.com>
2025-12-21 01:56:16 -0800

    fix(nav): remove fixed height and extra space for compact nav bar on all devices

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,17 +3,15 @@ import logo from "../assets/logo.jpg";
 
 const Nav = () => {
   return (
-    <nav className="relative bg-white w-full z-30">
-      <div className="mx-auto flex items-center justify-between h-20 container px-2 lg:px-4 max-w-screen-xl">
+    <nav className="bg-white w-full z-30">
+      <div className="mx-auto flex items-center justify-between py-2 px-2 lg:px-4 max-w-screen-xl">
         <a href="/">
           <img className="w-24" src={logo} alt="logo" />
         </a>
         <div className="flex items-center">
-          <div className="items-center mr-1 flex">
-            <a className="mx-2 lg:mx-4 hover:opacity-75 transition-all duration-300" href="">Docs</a>
-            <a className="mx-2 lg:mx-4 hover:opacity-75 transition-all duration-300 py-2 px-6 text-white bg-slate-900" href="https://discord.com/invite/ubaMrbUZxQ">Discord</a>
-          </div>
-          <div className="flex items-center">
+          <a className="mx-2 lg:mx-4 hover:opacity-75 transition-all duration-300" href="">Docs</a>
+          <a className="mx-2 lg:mx-4 hover:opacity-75 transition-all duration-300 py-2 px-6 text-white bg-slate-900" href="https://discord.com/invite/ubaMrbUZxQ">Discord</a>
+          <div className="flex items-center ml-2">
             <ConnectButton />
           </div>
         </div>
